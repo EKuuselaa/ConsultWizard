84204dc685d425075b87ddec4d1c09268a297974
EKuuselaa <esko.kuusela1@gmail.com>
2024-12-02 13:15:22 +0200

    Added css styling for the homepage.jsx

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
+import './HomePage.css';
 
 const HomePage = () => {
   return (
-    <div>
+    <div className="home-container">
       <h1>Tervetuloa Osaamisenhallinta-sovellukseen!</h1>
       <p>Tämä on etusivu. Voit navigoida konsulttien hallintaan tai käyttäjähallintaan yläpalkista.</p>
     </div>
